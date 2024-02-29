a4d49fb3cb60d9be9c372871ccb1c1f1c1d1c3b4
Asumu Takikawa <asumu@igalia.com>
2024-02-28 18:37:51 -0800

    Add updated Firefox harness

--- firefox/browser_spec-source-map.js
+++ firefox/browser_spec-source-map.js
@@ -9,11 +9,36 @@ async function isValidSourceMap(base) {
   return true;
 }
 
-add_task(async function testSourceMapVersion() {
+const SPEC_TESTS_URI = `${URL_ROOT_SSL}fixtures/source-map-spec-tests.json`
+const testDescriptions = JSON.parse(read(SPEC_TESTS_URI));
 
-  Assert.equal(true, await isValidSourceMap("version-valid"));
-  Assert.equal(false, await isValidSourceMap("version-not-a-number"));
-  Assert.equal(false, await isValidSourceMap("version-too-low"));
-  Assert.equal(false, await isValidSourceMap("version-too-high"));
+for (const testCase of testDescriptions.tests) {
+  async function testFunction() {
+      const baseName = testCase.baseFile.substring(0, testCase.baseFile.indexOf(".js"));
+      Assert.equal(testCase.sourceMapIsValid, await isValidSourceMap(baseName));
+  };
+  Object.defineProperty(testFunction, "name", { value: testCase.name });
+  add_task(testFunction);
+}
 
-});
+function read(srcChromeURL) {
+  const scriptableStream = Cc[
+    "@mozilla.org/scriptableinputstream;1"
+  ].getService(Ci.nsIScriptableInputStream);
+
+  const channel = NetUtil.newChannel({
+    uri: srcChromeURL,
+    loadUsingSystemPrincipal: true,
+  });
+  const input = channel.open();
+  scriptableStream.init(input);
+
+  let data = "";
+  while (input.available()) {
+    data = data.concat(scriptableStream.read(input.available()));
+  }
+  scriptableStream.close();
+  input.close();
+
+  return data;
+}
