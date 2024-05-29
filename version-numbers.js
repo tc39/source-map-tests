aa3ce8e7ee43a4a35aa6fd330b44c8bc9676e943
Asumu Takikawa <asumu@igalia.com>
2024-05-29 14:54:49 -0700

    Remove outdated files

--- version-numbers.js
+++
@@ -1,6 +0,0 @@
-// Test that invalid version numbers are rejected.
-
-assert(isValidSourceMap("./version-valid.map"));
-assert(!isValidSourceMap("./version-not-a-number.map"));
-assert(!isValidSourceMap("./version-too-low.map"));
-assert(!isValidSourceMap("./version-too-high.map"));
